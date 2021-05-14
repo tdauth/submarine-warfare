d9439808e5f19fe84f5c9866cc5b6612da7890bd
Barade <barade.barade@web.de>
2021-05-14 13:21:08 +0200

    Update script

--- updatemap.bat
+++ updatemap.bat
@@ -1,2 +1,2 @@
-copy C:\Users\Tamino\Documents\Projekte\submarine-warfare\submarinewarfare0.2.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+copy C:\Users\Tamino\Documents\Projekte\submarine-warfare\submarinewarfare0.3.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 pause
